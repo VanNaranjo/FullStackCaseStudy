4a23f194a2ef92622d3642d22f2486af3a8e74f7
DESKTOP-N6HP9TH\Van R <vannaranjo123@gmail.com>
2025-05-18 20:18:55 -0400

    Add project files.

---
+++ Controllers/Homeontroller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseStudy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Homeontroller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Index()
+        {
+            return "If you see this the server is up and running!";
+        }
+    }
+}
+ 
---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CaseStudy.DAL;
+
+using CaseStudy.DAL.DomainClasses;
+
+namespace CaseStudy.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public virtual DbSet<Product>? Products { get; set; }
+        public virtual DbSet<Brand>? Brands { get; set; }
+    }
+}
